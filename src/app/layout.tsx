89312437cc53fc5a5101e3b6240052a4561f95dd
sk1wz <78929376+sk1wz@users.noreply.github.com>
2026-07-10 18:25:07 +0300

    1

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,11 @@
 import type { Metadata } from "next";
 import "@/shared/styles/globals.css";
 import { ToastContainer } from "react-toastify";
-import { ThemeProvider } from "next-themes";
 import { roboto } from "@/shared/fonts/roboto";
 
 export const metadata: Metadata = {
-  title: "CRM",
-  description: "CRM frontend",
+  title: "InfoFusion",
+  description: "InfoFusion",
 };
 
 export default function RootLayout({
@@ -20,27 +19,20 @@ export default function RootLayout({
       suppressHydrationWarning
       className={`${roboto.variable}  h-full antialiased`}
     >
-      <body className="flex h-full w-full flex-col overflow-hidden">
-        <ThemeProvider
-          attribute="data-theme"
-          defaultTheme="light"
-          enableSystem
-          storageKey="theme"
-        >
-          <ToastContainer
-            position="bottom-right"
-            autoClose={1000}
-            hideProgressBar={false}
-            newestOnTop={false}
-            closeOnClick
-            rtl={false}
-            pauseOnFocusLoss={false}
-            draggable
-            pauseOnHover={false}
-            theme="light"
-          />
-          <div className="flex min-h-0 min-w-0 flex-1 flex-col">{children}</div>
-        </ThemeProvider>
+      <body className="m-0 flex h-full w-full flex-col overflow-hidden p-0">
+        <ToastContainer
+          position="bottom-right"
+          autoClose={1000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss={false}
+          draggable
+          pauseOnHover={false}
+          theme="light"
+        />
+        <div className="flex min-h-0 min-w-0 flex-1 flex-col">{children}</div>
       </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,38 @@
+import Image from "next/image";
 import Link from "next/link";
 
 export default function HomePage() {
   return (
-    <div>
-      home-page
-      <Link href="/login">Login</Link>
-    </div>
+    <main className="mx-auto flex min-h-screen w-full max-w-[1440px] flex-col">
+      <section className="relative w-full h-full max-h-[660px]">
+        <Image
+          src="/Group 3.png"
+          alt=""
+          aria-hidden
+          width={1440}
+          height={720}
+          priority
+          draggable={false}
+          className="pointer-events-none absolute left-0 block h-full w-full select-none object-cover"
+        />
+
+        <div className="relative z-10 flex items-center justify-between p-4 max-w-[1350px] mx-auto">
+          <Image
+            src="/logo.png"
+            alt="InfoFusion logo"
+            width={120}
+            height={120}
+            priority
+          />
+
+          <Link
+            href="/login"
+            className="rounded-xl bg-[#C8DDD5] px-4 py-3 text-sm text-center font-semibold text-[#3E3C4B] bg-linear-to-t from-[#C8DDD5] to-[#F3F3F3] shadow"
+          >
+            ЛИЧНЫЙ КАБИНЕТ
+          </Link>
+        </div>
+      </section>
+    </main>
   );
 }
