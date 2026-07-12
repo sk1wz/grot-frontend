4a50a3b8fdc3b610538ffd33511ae4ba69e4698b
sk1wz <78929376+sk1wz@users.noreply.github.com>
2026-07-12 23:22:29 +0300

    feat: update

--- src/shared/api/config.ts
+++ src/shared/api/config.ts
@@ -1,6 +1,8 @@
 const rawOrigin =
   process.env.NEXT_PUBLIC_API_ORIGIN ?? "https://api.ivatracker.ru";
 
-export const apiOrigin = rawOrigin.replace(/\/$/, "");
+export const apiOrigin = rawOrigin
+  .replace(/\/$/, "")
+  .replace(/^http:\/\/(api\.ivatracker\.ru(?::\d+)?)/, "https://$1");
 
 export const baseURL = `${apiOrigin}/api`;
