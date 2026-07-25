c24ccf6d3703e1f6686a3996cff94e46e9674297
sk1wz <78929376+sk1wz@users.noreply.github.com>
2026-07-25 10:10:55 +0300

    feat: Внедрить Header

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,4 +1,5 @@
 import { MainProvider } from "@/shared/providers";
+import { Header } from "@/widgets/header";
 import { Sidebar } from "@/widgets/sidebar";
 
 export default function DashboardLayout({
@@ -11,6 +12,7 @@ export default function DashboardLayout({
       <div className="flex h-full min-h-0 overflow-hidden">
         <Sidebar />
         <div className="flex min-h-0 min-w-0 flex-1 flex-col overflow-hidden">
+          <Header />
           <main className="min-h-0 flex-1 overflow-y-auto overflow-x-hidden p-2">
             {children}
           </main>
