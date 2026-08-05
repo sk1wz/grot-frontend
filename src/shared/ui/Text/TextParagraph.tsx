2f69abf02e7fb78c4cea6d1fbdc6f4646e7f0c18
sk1wz <78929376+sk1wz@users.noreply.github.com>
2026-08-05 23:48:35 +0300

    feat: update

--- src/shared/ui/Text/TextParagraph.tsx
+++ src/shared/ui/Text/TextParagraph.tsx
@@ -1,5 +1,5 @@
 export type TextParagraphProps = React.ComponentPropsWithoutRef<"p">;
 
 export function TextParagraph({ className = "", ...props }: TextParagraphProps) {
-  return <p className={`text-(--foreground) ${className}`} {...props} />;
+  return <p className={`text-(--foreground) text-xs sm:text-sm ${className}`} {...props} />;
 }
