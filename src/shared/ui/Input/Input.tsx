82a1fccffbe4c05dbdcca954e0f48d7c58112faa
sk1wz <78929376+sk1wz@users.noreply.github.com>
2026-08-05 23:08:53 +0300

    feat: update

--- src/shared/ui/Input/Input.tsx
+++ src/shared/ui/Input/Input.tsx
@@ -13,7 +13,7 @@ export const Input = forwardRef<HTMLInputElement, InputProps>(function Input(
       autoCorrect="off"
       autoCapitalize="off"
       spellCheck={spellCheck}
-      className={`w-full px-4 py-2.5 shadow-(--shadow-2) rounded-lg bg-(--field) text-(--foreground) outline-none disabled:opacity-50 disabled:cursor-not-allowed ${className}`}
+      className={`w-full text-sm px-4 py-2.5 shadow-(--shadow-2) rounded-lg bg-(--field) text-(--foreground) outline-none disabled:opacity-50 disabled:cursor-not-allowed ${className}`}
       {...props}
     />
   );
--- src/shared/ui/Select/Select.tsx
+++ src/shared/ui/Select/Select.tsx
@@ -7,7 +7,7 @@ export const Select = forwardRef<HTMLSelectElement, SelectProps>(
     return (
       <select
         ref={ref}
-        className={`w-full cursor-pointer rounded-lg bg-(--field) px-4 py-2 text-sm text-(--foreground) outline-none disabled:cursor-not-allowed disabled:opacity-50 ${className}`}
+        className={`w-full text-sm px-4 py-2.5 shadow-(--shadow-2) rounded-lg bg-(--field) text-(--foreground) outline-none disabled:opacity-50 disabled:cursor-not-allowed ${className}`}
         {...props}
       />
     );
