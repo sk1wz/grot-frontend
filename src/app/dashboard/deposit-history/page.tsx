02c7fa4753ead139f17c75b1a8d2d993cd53c7ed
sk1wz <78929376+sk1wz@users.noreply.github.com>
2026-08-07 00:59:49 +0300

    feat: update

--- src/app/dashboard/deposit-history/page.tsx
+++ src/app/dashboard/deposit-history/page.tsx
@@ -19,7 +19,7 @@ export default function DepositHistoryPage() {
         className="pointer-events-none select-none fixed top-0 right-0"
       />
 
-      <div className="border-4 p-8 border-[#d7e2ed] bg-white rounded-[100px_40px_100px_40px] relative">
+      <div className="md:border-4 md:p-8 md:border-[#d7e2ed] md:bg-white md:rounded-[100px_40px_100px_40px]relative">
         <DepositHistory />
       </div>
     </div>
--- src/app/dashboard/fssp/page.tsx
+++ src/app/dashboard/fssp/page.tsx
@@ -20,7 +20,7 @@ export default function FsspPage() {
         className="pointer-events-none select-none fixed top-0 right-0"
       />
 
-      <div className="border-4 p-8 border-[#d7e2ed] bg-white rounded-[100px_40px_100px_40px] relative">
+      <div className="md:border-4 md:p-8 md:border-[#d7e2ed] md:bg-white md:rounded-[100px_40px_100px_40px] relative">
         <CheckFormById configId="fssp" />
         <ChecksHistory module={CheckModule.FSSP} />
       </div>
